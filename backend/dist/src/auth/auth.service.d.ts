dad7095680252e6c24017cd9f7e9bba9b6cfc696
Pmoreamo <pau.anand.morello@gmail.com>
2026-06-08 14:30:33 +0200

    PrismaServices_reset + guide person B

--- backend/dist/src/auth/auth.service.d.ts
+++ backend/dist/src/auth/auth.service.d.ts
@@ -1,6 +1,2 @@
-import { PrismaService } from "../prisma.service";
 export declare class AuthService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    UserRegistration(email: string, name: string): Promise<any>;
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,15 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class AuthService {
-    constructor(private prisma: PrismaService) {}
-    async UserRegistration(email: string, name: string){
-        return this.prisma.user.create({
-            data: {
-                email: email,
-                name: name,
-            },
-        });
-    }
-}
+export class AuthService {}
